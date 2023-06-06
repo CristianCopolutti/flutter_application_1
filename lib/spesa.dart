dc39a1394d05176617f0e9216e04e18d827f377c
Cristian Copolutti <cristian.copolutti97@gmail.com>
2023-06-06 12:03:08 +0200

    aggiornamento 12:03

--- lib/spesa.dart
+++ lib/spesa.dart
@@ -8,7 +8,7 @@ class SpesaScreen extends StatefulWidget{
   State<StatefulWidget> createState() => _SpesaScreenState();
 }
 
-class _SpesaScreenState extend State<SpesaScreen>{
+class _SpesaScreenState extends State<SpesaScreen>{
 
 /*classe che viene utilizzata per gestire e controllare il testo in un widget 'TextField' o 'TextFormField'; questa classe fornisce metodi per accedere e modificare il testo all'interno del campo di testo
 */
@@ -42,9 +42,58 @@ class _SpesaScreenState extend State<SpesaScreen>{
         centerTitle: true,
       ),
       //body --> corpo principale dell'applicazione, contiene contenuto principale come testo, immagini, elenchi o altri widget
-      /* column: widget 
+      /* column: widget che consente di organizzare i suoi widget figlio in una colonna verticale, uno sopra l'altro; utile quando si vuole impilare widget verticalmente all'interno di un layout
+      */
       body: Column(
-        children: [],
+        /*children viene utilizzato per definire un elenco di widget figlio da visualizzare o posizionare all'interno del widget padre; accetta un elenco di widget come valore
+        */
+        children: [
+          /*
+          widget Padding utilizzato per aggiungere spaziatura interna ai suoi widget figlio; consente di impostare uno spazio vuoto uniforme o specifico sui 4 lati dei widget figlio
+          */
+          Padding(
+            padding: EdgeInsets.all(10.0),
+            /*
+            widget row viene utilizzato per organizzare i widget figlio in una riga orizzontale, allineandoli uno accanto all'altro
+            */
+            child: Row(
+              children: [
+                /*
+                widget expanded --> utilizzato per definire un widget che si espande per occupare lo spazio disponibile all'interno del widget genitore.
+                Il widget all'interno di expanded si espanderà o si contrarrà a seconda dello spazio disponibile
+                */
+                Expanded(
+                  child: TextField(
+                    controller: nomeController,
+                    decoration: InputDecoration(labelText: 'Nome prodotto'),
+                  )),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Expanded(
+                  child: TextField(
+                    controller: quantitaController,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(labelText: 'Quantità'),
+                  )),
+                SizedBox(
+                  width: 10.0,
+                ),
+                /*
+                
+                */
+                DropdownButton<String>(
+                  value: unitaSelezionata, 
+                  onChanged: (String? nuovoValore){
+                    setState(() {
+                      unitaSelezionata = nuovoValore!;
+                    });
+                  },
+                  items: [],)
+              ],
+            ),
+          )
+        ],
       ),
     )
   }
