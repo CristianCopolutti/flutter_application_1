ac72334b1c4c8ed74430d7bba98c6d3b6e42b4fd
Cristian Copolutti <cristian.copolutti97@gmail.com>
2023-06-06 11:22:38 +0200

    aggiornamento 11:22

--- lib/shoppinglistprovider.dart
+++ lib/shoppinglistprovider.dart
@@ -6,6 +6,7 @@ import 'package:flutter_application_1/product.dart';
 - definizione provider --> contiene i dati da condividere; classe personalizzata che estende ChangeNotifier
 - consumo del provider --> all'interno del widget figlio si può accedere ai dati forniti dal provider utilizzando il widget Consumer; questo widget automaticamente accede ai cambiamenti dei dati forniti e si aggiorna quando i dati cambiano
 - quando i dati all'interno del provider cambiano si possono chiamare i metodi 'notifyListeners per ChangeNotifier per informare i widget che i dati sono stati modificati
+- Provider.of --> permette di accedere ai dati senza dover specificare esplicitamente l'abbonamento ai cambiamenti
 */
 
 class ShoppingListProvider extends ChangeNotifier {
--- lib/spesa.dart
+++ lib/spesa.dart
@@ -1,152 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/product.dart';
+import 'package:flutter_application_1/shoppinglistprovider.dart';
 import 'package:provider/provider.dart';
 
-class ListaSpesaScreen extends StatelessWidget {
+class SpesaScreen extends StatefulWidget{
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+  State<StatefulWidget> createState() => _SpesaScreenState();
+}
+
+class _SpesaScreenState extend State<SpesaScreen>{
+
+/*classe che viene utilizzata per gestire e controllare il testo in un widget 'TextField' o 'TextFormField'; questa classe fornisce metodi per accedere e modificare il testo all'interno del campo di testo
+*/
+  TextEditingController nomeController = TextEditingController();
+  TextEditingController quantitaController = TextEditingController();
+
+  String unitaSelezionata = '';
+
+  void aggiungiProdotto(){
+    String nome = nomeController.text;
+    double quantita = double.parse(quantitaController.text);
+
+    if (nome.isNotEmpty && quantita > 0 && unitaSelezionata.isNotEmpty){
+      Product nuovoProdotto = Product(name: nome, quantita: quantita, unit: unitaSelezionata);
+
+    //aggiunge un nuovo prodotto inerente al contesto attualmente in uso
+      Provider.of<ShoppingListProvider>(context, listen: false).aggiungiProdotto(nuovoProdotto);
+
+      nomeController.clear();  //metodo utilizzato per cancellare il testo
+      quantitaController.clear();
+      unitaSelezionata = '';
+    }
+
+  }
+
+  @override
+  Widget build(BuildContext context) { //BuildContext context --> contesto corrente in cui viene costruito il widget; il metodo build deve restituire un widget che rappresenta la rappresentazione visuale del widget corrente
+    return Scaffold( //fornisce struttura di base
       appBar: AppBar(
-        title: Text('Lista della Spesa'),
+        title: Text('Lista della spesa'),
+        centerTitle: true,
       ),
-      body: Consumer<ListaSpesa>(
-        builder: (context, listaSpesa, _) {
-          return ListView.builder(
-            itemCount: listaSpesa.prodotti.length,
-            itemBuilder: (context, index) {
-              final prodotto = listaSpesa.prodotti[index];
-              return Dismissible(
-                key: Key(prodotto.descrizione),
-                onDismissed: (direction) {
-                  listaSpesa.rimuoviProdotto(index);
-                },
-                child: ListTile(
-                  title: Text(prodotto.descrizione),
-                  subtitle: Text('Quantità: ${prodotto.quantita}'),
-                  trailing: Checkbox(
-                    value: prodotto.acquistato,
-                    onChanged: (value) {
-                      listaSpesa.setAcquistato(index, value ?? false);
-                    },
-                  ),
-                ),
-              );
-            },
-          );
-        },
+      //body --> corpo principale dell'applicazione, contiene contenuto principale come testo, immagini, elenchi o altri widget
+      /* column: widget 
+      body: Column(
+        children: [],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showDialog(
-            context: context,
-            builder: (BuildContext context) {
-              final TextEditingController _descrizioneController =
-                  TextEditingController();
-              final TextEditingController _quantitaController =
-                  TextEditingController();
-              final TextEditingController _alternativaController =
-                  TextEditingController();
-              return AlertDialog(
-                title: Text('Aggiungi Prodotto'),
-                content: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    TextField(
-                      controller: _descrizioneController,
-                      decoration: InputDecoration(labelText: 'Descrizione'),
-                    ),
-                    TextField(
-                      controller: _quantitaController,
-                      keyboardType: TextInputType.number,
-                      decoration: InputDecoration(labelText: 'Quantità'),
-                    ),
-                    Consumer<ListaSpesa>(
-                      builder: (context, listaSpesa, _) {
-                        return DropdownButton<String>(
-                          itemHeight: null,
-                          value: _alternativaController.text,
-                          onChanged: (String? newValue) {
-                            _alternativaController.text = newValue ?? '';
-                          },
-                          isExpanded: true,
-                          items: listaSpesa.alternative
-                              .map<DropdownMenuItem<String>>((String value) {
-                            return DropdownMenuItem<String>(
-                              value: value,
-                              child: Text(value),
-                            );
-                          }).toList(),
-                        );
-                      },
-                    ),
-                  ],
-                ),
-                actions: [
-                  TextButton(
-                    child: Text('Annulla'),
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                  ),
-                  TextButton(
-                    child: Text('Aggiungi'),
-                    onPressed: () {
-                      final nuovoProdotto = Prodotto(
-                        descrizione: _descrizioneController.text,
-                        quantita: int.tryParse(_quantitaController.text) ?? 0,
-                        alternativa: _alternativaController.text,
-                      );
-                      final listaSpesa =
-                          Provider.of<ListaSpesa>(context, listen: false);
-                      listaSpesa.aggiungiProdotto(nuovoProdotto);
-
-                      _descrizioneController.clear();
-                      _quantitaController.clear();
-                      _alternativaController.clear();
-                      Navigator.of(context).pop();
-                    },
-                  ),
-                ],
-              );
-            },
-          );
-        },
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-}
-
-class ListaSpesa with ChangeNotifier {
-  List<Prodotto> prodotti = [];
-  List<String> alternative = ['Opzione 1', 'Opzione 2', 'Opzione 3'];
-
-  void aggiungiProdotto(Prodotto prodotto) {
-    prodotti.add(prodotto);
-    notifyListeners();
+    )
   }
 
-  void rimuoviProdotto(int index) {
-    prodotti.removeAt(index);
-    notifyListeners();
-  }
-
-  void setAcquistato(int index, bool value) {
-    prodotti[index].acquistato = value;
-    notifyListeners();
-  }
-}
-
-class Prodotto {
-  String descrizione;
-  int quantita;
-  String alternativa;
-  bool acquistato;
-
-  Prodotto({
-    required this.descrizione,
-    required this.quantita,
-    required this.alternativa,
-    this.acquistato = false,
-  });
 }
